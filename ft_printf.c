317027d1a5b0826b34574d288bcb3b5a1f713097
denisgodoy <denisgodoy1@gmail.com>
2021-09-08 19:21:34 -0300

    update ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 23:22:40 by degabrie          #+#    #+#             */
-/*   Updated: 2021/09/08 19:10:08 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/09/08 19:20:23 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,25 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	while (format[i])
 	{
-		if (format[i] == '%' && ft_strchr("cspdiuxX", format[i +1]))
+		if (format[i] == '%' && ft_strchr("spdiuxX%", format[i + 1]))
 		{
 			if (format[i + 1] == 'd')
 			{
 				temp = ft_itoa(va_arg(args, int));
-				size += ft_strlen(temp);
 				ft_putstr_fd(temp, 1);
+				size += ft_strlen(temp);
 				free(temp);
 			}
 			else if (format[i + 1] == 's')
 			{
 				temp = va_arg(args, char *);
-				size += ft_strlen(temp);
 				ft_putstr_fd(temp, 1);
+				size += ft_strlen(temp);
+			}
+			else if (format[i + 1] == '%')
+			{
+				ft_putchar_fd('%', 1);
+				size++;
 			}
 			i++;
 		}
@@ -58,7 +63,7 @@ int	ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	int ret = ft_printf(" %d %d %s ", 10, 20, "hello");
+	int ret = ft_printf(" %d %% %s", 10, "hello");
 	ft_printf("\n%d", ret);
 	return (0);
 }
