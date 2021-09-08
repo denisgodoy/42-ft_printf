cf9ece9ee582b5b5d9d9445c8d9b647dfe6b976e
denisgodoy <denisgodoy1@gmail.com>
2021-09-08 19:11:43 -0300

    update ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 23:22:40 by degabrie          #+#    #+#             */
-/*   Updated: 2021/09/07 18:23:57 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/09/08 19:10:08 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,15 @@
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
-	char	*value;
 	char	*temp;
-	char	new_arr[100];
 	size_t 	i;
-	size_t 	j;
+	size_t	size;
 
+	size = 0;
 	if (!format)
 		return (-1);
-	if (!ft_strchr(format, '%'))
-	{
-		ft_putstr_fd((char *)format, 1);
-		return (ft_strlen((char *)format));
-	}
 	va_start(args, format);
 	i = 0;
-	j = 0;
 	while (format[i])
 	{
 		if (format[i] == '%' && ft_strchr("cspdiuxX", format[i +1]))
@@ -40,37 +33,32 @@ int	ft_printf(const char *format, ...)
 			if (format[i + 1] == 'd')
 			{
 				temp = ft_itoa(va_arg(args, int));
-				ft_strlcpy(&new_arr[j], temp, ft_strlen(temp) + ft_strlen(new_arr));
-				j += ft_strlen(temp);
+				size += ft_strlen(temp);
+				ft_putstr_fd(temp, 1);
 				free(temp);
 			}
 			else if (format[i + 1] == 's')
 			{
-				value = va_arg(args, char *);
-				ft_strlcpy(&new_arr[j], value, ft_strlen(value) + ft_strlen(new_arr));
-				j += ft_strlen(value);
+				temp = va_arg(args, char *);
+				size += ft_strlen(temp);
+				ft_putstr_fd(temp, 1);
 			}
-			i += 2;
+			i++;
 		}
-		if (i > ft_strlen(format))
-			break ;
 		else
-			new_arr[j++] = format[i++];
+		{
+			ft_putchar_fd(format[i], 1);
+			size++;	
+		}
+		i++;
 	}
 	va_end(args);
-	ft_putstr_fd(new_arr, 1);
-	ft_putnbr_fd(ft_strlen(new_arr), 1);
-	return (ft_strlen(new_arr));
+	return (size);
 }
 
 int	main(void)
 {
-	char	*str;
-	int ret;
-	
-	str = "olá mundo";
-	int n = INT_MIN;
-	int nbr = INT_MAX;
-	ft_printf("OLÁ MUNDO %d %s", n, str);
+	int ret = ft_printf(" %d %d %s ", 10, 20, "hello");
+	ft_printf("\n%d", ret);
 	return (0);
 }
