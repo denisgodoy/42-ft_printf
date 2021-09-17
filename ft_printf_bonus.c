0217f2b59b7aa15ddf385b80df213a0cb229b9b3
Denis Godoy <denisgoody1@gmail.com>
2021-09-17 17:53:08 -0300

    update ft_printf bonus

--- ft_printf_bonus.c
+++ ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 23:22:40 by degabrie          #+#    #+#             */
-/*   Updated: 2021/09/16 10:09:34 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/09/17 16:13:31 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,10 @@ static int	ft_convert(const char **format, int *size, va_list args, int i)
 		j = i;
 		if (ft_strchr("# +", (*format)[i + 1]))
 		{
-			while (!ft_strchr("sdixX", (*format)[j++]))
-			{
-				*size += ft_flag_format((*format)[i + 1], (*format)[j], args);
-				if (ft_isdigit((*format)[j]))
-				{
-					ft_putchar_fd(' ', 1);
-					*size += 1;
-				}
-			}
-			i = j - 2;
+			while (!ft_strchr("dixX", (*format)[++j]))
+				continue ;
+			*size += ft_flag_format((*format)[i + 1], (*format)[j], args);
+			i = (j - 1);
 		}
 		else
 			*size += ft_format((*format)[i + 1], args);
